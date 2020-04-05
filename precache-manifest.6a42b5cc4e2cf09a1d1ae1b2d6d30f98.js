f72db2d63e211c0fea1eea43784d85d13b7339c5
Mark Sun <sunmark14@gmail.com>
2020-04-05 08:30:24 -0400

    Updates

---
+++ precache-manifest.6a42b5cc4e2cf09a1d1ae1b2d6d30f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6db1f0efc9746f4aa96bd1331d7bf25e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0c0b9c1da9bc6d02c9df",
+    "url": "/monsters-rolodex/static/css/main.6a322936.chunk.css"
+  },
+  {
+    "revision": "8a6eda421bc5765c55c2",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bc65f737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0b9c1da9bc6d02c9df",
+    "url": "/monsters-rolodex/static/js/main.56be8282.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
